452fdff2090abbd3bac4f6167b4dad238efc3d97
Evanibaldo <reavaniunu@gmail.com>
2025-04-06 16:00:46 -0600

    Add files via upload

---
+++ codigo/conexion.py
@@ -0,0 +1,19 @@
+import pyodbc
+
+def obtener_conexion_gestion_usuario():
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=localhost;'
+        'DATABASE=FM_Clientes;'
+        'Trusted_Connection=yes;'
+    )
+    return conn
+
+def obtener_conexion_productos():
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'SERVER=localhost;'
+        'DATABASE=FM_Productos;'
+        'Trusted_Connection=yes;'
+    )
+    return conn
